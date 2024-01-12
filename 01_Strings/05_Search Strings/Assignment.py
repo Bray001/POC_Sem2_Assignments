beef6b388fc25a47f1be394dd8c372bf0b7ab5f1
Braylon McDaniel <mcdanbra001@warren.k12.in.us>
2024-01-12 08:09:30 -0500

    commit

---
+++ 01_Strings/05_Search Strings/Assignment.py
@@ -0,0 +1,14 @@
+text = input("store text")
+
+result = text.find("the")
+
+if result == -1:
+    print("The word the is not in the string")
+else:
+    print("The word the is in the string")
+    print("the word the appears at", result)
+
+
+
+
+
