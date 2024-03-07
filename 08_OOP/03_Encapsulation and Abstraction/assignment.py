8c184834322a75fae3d7cd5fc32005feb370e1eb
Braylon McDaniel <mcdanbra001@warren.k12.in.us>
2024-03-07 07:56:23 -0500

    updates

--- 08_OOP/03_Encapsulation and Abstraction/assignment.py
+++ 08_OOP/03_Encapsulation and Abstraction/assignment.py
@@ -6,14 +6,25 @@ class Rectangle:
     def get_height(self) -> float:
         return self.__height
     
-    #YOUDO the get_base method
+    def get_base(self) -> float:
+        return self.__base
     
     def get_perimeter(self) -> float:
         return 2 * self.__base + 2 * self.__height
     
-    #Youdo get_area method
+    def get_area(self) -> float:
+        return 2 * self._ + 2 * self.__height 
  
  
- 
-#YOUDO>  create two rectangles.  print their base, height, perimeter, and area
-#using only the methods not the fields/property/attributes
+rectangle1 = Rectangle (4,3) 
+print("the base of rectangle is", rectangle1.get_base)
+print("the height of rectangle is", rectangle1.get_height)
+print("The perimeter is", rectangle1.get_perimeter)
+print("The area pf rectange is", rectangle1.get_area.)
+
+rectangle2 = Rectangle (2,5)
+print("the base of rectangle is", rectangle2.get_base)
+print("the height of rectangle is", rectangle2.get_height)
+print("The perimeter is", rectangle2.get_perimeter)
+print("The area pf rectange is", rectangle2.get_area.)
+
