3b75eff54fec76bc5f6b6baccbf55490324c02e0
Braylon McDaniel <mcdanbra001@warren.k12.in.us>
2024-03-08 07:49:36 -0500

    updates

--- 08_OOP/04_Class and Instance Variables/assignment.py
+++ 08_OOP/04_Class and Instance Variables/assignment.py
@@ -1,6 +1,9 @@
 class Rectangle:
     
-    
+    counter = 0
+
+    def get_count():
+        return Rectangle._counter
     
     def __init__(self, base: float, height: float) -> None:
         if(base < 0):
@@ -15,14 +18,31 @@ class Rectangle:
         
     def get_height(self) -> float:
         return self.__height
+
+     def get_base(self) -> float:
+        return self.__base
     
-    #YOUDO the get_base method
     
     def get_perimeter(self) -> float:
         return 2 * self.__base + 2 * self.__height
     
-    #Youdo get_area method
+    def get_area(self) -> float:
+        return 2 * self.__base + 2 * self.__height
+    
  
+ rectangle1 = Rectangle (4,3) 
+print("the base of rectangle is", rectangle1.get_base)
+print("the height of rectangle is", rectangle1.get_height)
+print("The perimeter is", rectangle1.get_perimeter)
+print("The area of rectange is", rectangle1.get_area.)
+
+rectangle2 = Rectangle (2,5)
+print("the base of rectangle is", rectangle2.get_base)
+print("the height of rectangle is", rectangle2.get_height)
+print("The perimeter is", rectangle2.get_perimeter)
+print("The area of rectange is", rectangle2.get_area.)
+
+
  
  
 #YOUDO>  create two rectangles.  print their base, height, perimeter, and area
