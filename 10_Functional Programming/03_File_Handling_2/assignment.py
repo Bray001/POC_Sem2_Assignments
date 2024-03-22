bbd53ac708469a722e30d5ee10a915961b2b8787
Braylon McDaniel <mcdanbra001@warren.k12.in.us>
2024-03-22 07:58:15 -0400

    commit

--- 10_Functional Programming/03_File_Handling_2/assignment.py
+++ 10_Functional Programming/03_File_Handling_2/assignment.py
@@ -1,1 +1,7 @@
 file_path = "C:\\Users\\Nicholas Armstrong\\Development\\POC_Sem2_Assignments\\10_Functional Programming\\03_File_Handling_2\\newfile.txt"
+
+stream = open(file_path)
+stream.write("This is the first message!\n")
+stream.write('This is my second message!')
+stream.write("This is my second message!")
+stream.close
