40dae83e9d919da2db3e3a69f3f74f96a5a75145
Braylon McDaniel <mcdanbra001@warren.k12.in.us>
2024-03-22 07:42:28 -0400

    update

--- 10_Functional Programming/03_File Handling/assignment.py
+++ 10_Functional Programming/03_File Handling/assignment.py
@@ -1,4 +1,8 @@
 file_path = "C:\\Users\\Nicholas Armstrong\\Development\\POC_Sem2_Assignments\\10_Functional Programming\\03_File Handling\myFile.txt"
 
-##YOUDO rest of the program for reading and printing to the console
-##remember pass in file_path to open 
+try:
+    stream= open("file_patch")
+    print(stream.read())
+     stream.close()
+except Exception as e:
+    print("an error occured", e)
